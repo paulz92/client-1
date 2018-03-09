8d4c7a3d9d62f97b0195f66ad50907f74f79ba55
Paul Zavattieri <paulz92@gmail.com>
2018-03-09 14:42:43 -0500

    card refactor

--- components/CarCard/index.js
+++ components/CarCard/index.js
@@ -1,4 +1,5 @@
-import Card, { CardContent, CardMedia } from 'material-ui/Card'
+import Card, { CardContent, CardMedia, CardHeader } from 'material-ui/Card'
+import Avatar from 'material-ui/Avatar'
 import Typography from 'material-ui/Typography'
 import Chip from 'material-ui/Chip'
 import Button from 'material-ui/Button'
@@ -8,13 +9,19 @@ import styles from './index.scss'
 export const CarCard = (props) => {
   return (
     <Card className={styles.carCardRoot}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="profpic" className={styles.avatarPic}>
+            R
+          </Avatar>
+        }
+        title={props.owner}
+        subheader="March 19, 2018"
+      />
       <CardMedia className={styles.carPic} image={`${props.carPic}`} title={`image of ${props.carMake} ${props.carModel}`} />
       <CardContent className={styles.carCardContent}>
         <Typography className={styles.carInfo} variant="headline" >
           {`${props.carYear} ${props.carMake} ${props.carModel}`}
-        </Typography>
-        <Typography className={styles.carInfo} component="p">
-          {`Owned by: ${props.owner}`}
         </Typography>
         <Typography className={styles.carInfo} component="p">
           {props.carNote}
@@ -28,8 +35,9 @@ export const CarCard = (props) => {
         {`Price: $${props.price}`} 
       </div>
       */}
-      <div>
+      <div className={styles.carButtonRoot}>
         <Button variant="raised" className={styles.carLikeButton}>{props.carAction}</Button>
+        <Button variant="raised" className={styles.carLikeButton}>Comment</Button>
       </div>
     </Card>
   )
