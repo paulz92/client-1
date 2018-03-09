f8053c729839cb261c1bae0982113f9466e3bb4d
Charles Kenney <Charlesc.kenney@gmail.com>
2018-03-09 12:55:33 -0500

    add responseive header

--- containers/Layout/Header/index.js
+++ containers/Layout/Header/index.js
@@ -1,21 +1,45 @@
+import Link from 'next/link'
 import { AppBar, Toolbar, Typography, Button, IconButton } from 'material-ui'
 import MenuIcon from 'material-ui-icons/Menu'
 import MoreIcon from 'material-ui-icons/MoreVert'
 
 import styles from './index.scss'
 
+const HeaderLink = ({ to, label }) => (
+  <Link href={to}>
+    <Typography className={styles.link}>
+      {label}
+    </Typography>
+  </Link>
+)
+
+const HeaderButton = ({ onClick, label }) => (
+  <Button
+    onClick={onClick}
+    className={styles.button}
+    variant="raised"
+  >
+    {label}
+  </Button>
+)
+
 export const Header = () => (
   <AppBar className={styles.appBar} position="static">
-    <Toolbar className={styles.toolBar}>
-      <IconButton className={styles.menuButton} aria-label="Menu">
-        <MenuIcon />
-      </IconButton>
-      <Typography className={styles.title}>
-        Torq
-      </Typography>
-      <IconButton className={styles.moreButton}>
-        <MoreIcon />
-      </IconButton>
+    <Toolbar className={styles.topToolBar}>
+      <HeaderButton label="REGISTER" />
+      <HeaderButton label="LOGIN" />
+    </Toolbar>
+    <Toolbar className={styles.bottomToolBar}>
+      <HeaderLink to="/" label="CARS" />
+      <HeaderLink to="/marketplace" label="MARKETPLACE" />
+      <Typography className={styles.brand}>Torq</Typography>
+      <HeaderLink to="/meetups" label="MEETUPS" />
+      <HeaderLink to="/about" label="ABOUT" />
+    </Toolbar>
+    <Toolbar className={styles.mobileToolBar}>
+      <IconButton><MenuIcon/></IconButton>
+      <Typography className={styles.brand}>Torq</Typography>
+      <IconButton><MoreIcon/></IconButton>
     </Toolbar>
   </AppBar>
 )
