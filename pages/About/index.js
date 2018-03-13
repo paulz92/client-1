c7823221be175a53b12edf037b22e580f37c3abb
Paul Zavattieri <paulz92@gmail.com>
2018-03-13 17:23:25 -0400

    reusable components for bios

--- pages/About/index.js
+++ pages/About/index.js
@@ -1,100 +1,47 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
-import Typography from 'material-ui/Typography';
-import Avatar from 'material-ui/Avatar'
 
 import { Layout } from '@/containers'
 import styles from './index.scss'
-import { AboutPage, Title } from '@/components';
+import { AboutPage, Title, AboutMe } from '@/components';
 
-const CHARLES_IMG = '/public/images/cuteCharles.png'
-const PAUL_IMG = '/public/images/paul-z2.jpeg'
-const EMMANUEL_IMG = '/public/images/eman.jpg'
-const PLACEHOLDER_AVATAR = '/public/images/user-placeholder.svg'
+const developers = [
+  {
+    name: "Charles Kenney",
+    role: "Backend Developer",
+    bio: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica.",
+    pic: "/public/images/cuteCharles.png"
+  },
+  {
+    name: "Paul Zavattieri",
+    role: "Frontend Developer",
+    bio: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica.",
+    pic: "/public/images/paul-z2.jpeg"
+  },
+  {
+    name: "Emmanuel M.R.",
+    role: "Frontend Developer",
+    bio: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica.",
+    pic: "/public/images/eman.jpg"
+  },
+  {
+    name: "Jamal Patterson",
+    role: "Frontend Developer",
+    bio: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica.",
+    pic: "/public/images/user-placeholder.svg"
+  }
+];
+
 export default () => (
   <Layout>
     <div >
-    <Title title="About Torq" />
-    <Title title="Our Team" />
-    <div className={styles.root}>
-    {/* card one */}
-      <Card className={styles.cardRoot}>
-        <Avatar
-          className={styles.avatar}
-          src={CHARLES_IMG}
-        />
-        <CardContent>
-          <Typography className={styles.font} variant="headline" component="h1">
-            Charles Kenney
-          </Typography>
-          <Typography className={styles.font} component="h2">
-            Backend Developer
-          </Typography>
-          <Typography className={styles.bio} component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </Card>
-      {/* card two */}
-      <Card className={styles.cardRoot}>
-        <Avatar
-          className={styles.avatar}
-          src={PAUL_IMG}
-        />
-        <CardContent>
-          <Typography className={styles.font} variant="headline" component="h1">
-            Paul Z
-          </Typography>
-          <Typography className={styles.font} component="h2">
-            Developer
-          </Typography>
-          <Typography className={styles.bio} component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </Card>
-      {/* Card Three */}
-      <Card className={styles.cardRoot}>
-        <Avatar
-          className={styles.avatar}
-          src={EMMANUEL_IMG}
-        />
-        <CardContent>
-          <Typography className={styles.font} variant="headline" component="h1">
-            Emmanuel M.R.
-          </Typography>
-          <Typography className={styles.font} component="h2">
-            Developer
-          </Typography>
-          <Typography className={styles.bio} component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </Card>
-      {/* Card Four */}
-      <Card className={styles.cardRoot}>
-        <Avatar
-          className={styles.avatar}
-          src={PLACEHOLDER_AVATAR}
-        />
-        <CardContent>
-          <Typography className={styles.font} variant="headline" component="h1">
-            Jamal P
-          </Typography>
-          <Typography className={styles.font} component="h2">
-            Developer
-          </Typography>
-          <Typography className={styles.bio} component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </Card>
+      <Title title="About Torq" />
+      <Title title="Our Team" />
+      <div className={styles.root}>
+        {developers.map(developer => {
+          return (
+            <AboutMe devName={developer.name} devRole={developer.role} devBio={developer.bio} devPic={developer.pic} />
+          )
+        })}
       </div>
     </div>
   </Layout>
