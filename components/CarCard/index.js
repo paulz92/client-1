f834618fba7d761482e368eda4d05d3b330a28af
Paul Zavattieri <paulz92@gmail.com>
2018-03-18 15:49:22 -0400

    car comment modal updates

--- components/CarCard/index.js
+++ components/CarCard/index.js
@@ -1,5 +1,5 @@
 import Card, { CardContent, CardMedia, CardHeader } from 'material-ui/Card'
-import { Avatar, Typography, Chip, Button, Modal, FormControl, TextField } from 'material-ui'
+import { Avatar, Typography, Chip, Button } from 'material-ui'
 import Comment from 'material-ui-icons/Comment'
 import ThumbsUp from 'material-ui-icons/ThumbUp'
 
@@ -8,34 +8,8 @@ import styles from './index.scss'
 export const CarCard = (props) => {
   const carActions = (
     <div>
-      <ThumbsUp className={styles.carAction + " " + styles.carActionLike} onClick={props.handleLike} />
+      <ThumbsUp focusable className={styles.carAction + " " + styles.carActionLike} onClick={props.handleLike} />
       <Comment className={styles.carAction} onClick={props.openModal} />
-      <Modal
-        className={styles.modalRoot}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-        open={props.shouldModalBeOpen}
-        onClose={props.closeModal}
-      >
-        <div className={styles.getModalStyles}>
-          <Typography className={styles.modalTitle} variant="title">
-            {`${props.carYear} ${props.carMake} ${props.carModel}`}
-          </Typography>
-          <img className={styles.modalPic} src={`${props.carPic}`} alt='carpic' />
-          <Typography className={styles.carNote} variant="title">
-            {props.carNote}
-          </Typography>
-          <FormControl className={styles.commentBody} onChange={props.changed}>
-            <TextField
-              label="Add Comment"
-              name="comment"
-              // value={props.newPostInfo.body}
-              multiline
-              rows="3"
-              margin="normal" />
-          </FormControl>
-        </div>
-      </Modal>
     </div>
   )
 
